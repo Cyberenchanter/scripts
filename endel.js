f454485a95de84978cd6d0e5b7937c9383148935
Cpphut <admin@cpphut.net>
2021-06-29 19:12:55 -0700

    Rm useless.

--- endel.js
+++
@@ -1,8 +0,0 @@
-let body = JSON.parse($response.body)
-if (body.subscription) {
-    body.subscription.type = "ACTIVE"
-    body.subscription.store = "APP_STORE"
-    $done({body:JSON.stringify(body)})
-} else {
-    $done({})
-}
