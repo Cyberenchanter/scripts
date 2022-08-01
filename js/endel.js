562939129c447ee6b9e7f23a475dbef81e54e4f2
Alexei Stukov <admin@cpphut.com>
2022-08-01 12:45:36 +0800

    Update Endel

--- js/endel.js
+++ js/endel.js
@@ -1,6 +1,8 @@
 let body = JSON.parse($response.body)
 if (body.subscription) {
     body.subscription.type = "ACTIVE"
+    body.subscription.time_left = 999999
+    body.subscription.valid_until = 4089196800
     body.subscription.store = "APP_STORE"
     $done({body:JSON.stringify(body)})
 } else {
